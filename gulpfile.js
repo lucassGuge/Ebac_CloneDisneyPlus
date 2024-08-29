7a9dc04149aaf9919d2c22dfdeb4e39b2055fffb
GugeLucas <lucas.gugelmin@gmail.com>
2024-08-28 21:29:32 -0400

    Update
    faq-question

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,26 @@ const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 
-
-
-
+// Função para compilar estilos
 function styles() {
     return gulp.src('./src/styles/**/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe (gulp.dest('./dist/css'));
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
 }
+
+// Função para otimizar imagens
 function images() {
     return gulp.src('./src/images/**/*')
-    .pipe(imagemin())
-    .pipe (gulp.dest('./dist/images'));
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
+
+// Função para minificar scripts
 function scripts() {
-    return gulp.src('./src/script/*.js') // Seleciona todos os arquivos JS
-        .pipe(uglify()) // Comprime os arquivos
-        .pipe(rename({ extname: '.min.js' })) // Renomeia para .min.js
-        .pipe(gulp.dest('./dist/js')); // Salva na pasta de destino
+    return gulp.src('./src/script/**/*.js') // Corrigido para observar todos os arquivos JS
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(gulp.dest('./dist/js'));
 }
 
 // Exportar a tarefa padrão
@@ -31,4 +33,5 @@ exports.default = gulp.parallel(styles, scripts, images);
 exports.watch = function() {
     gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles));
     gulp.watch('./src/script/**/*.js', gulp.parallel(scripts)); // Adiciona observação para scripts
+    gulp.watch('./src/images/**/*', gulp.parallel(images)); // Adiciona observação para imagens
 };
--- src/script/main.js
+++ src/script/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Seleciona todos os botões de abas
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     // Adiciona um evento de clique a cada botão
     buttons.forEach(button => {
@@ -22,8 +23,18 @@ document.addEventListener("DOMContentLoaded", function() {
             event.target.classList.add('shows__tabs__button--is-active');
         });
     });
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 });
 
+    function abreOuFechaResposta(elemento){
+        const classe = 'faq__questions__item--is-open';
+        console.log(elemento);
+        const elementoPai =elemento.target.parentNode;
+
+        elementoPai.classList.toggle(classe);
+    }
 // Função para remover a classe ativa de todos os botões
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
